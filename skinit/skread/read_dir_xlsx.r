36d83dca2c2be759ba569fbf996a57ab2a3d6ce5
simonsteiger <simon.j.steiger@gmail.com>
2026-03-20 11:18:55 +0100

    multi-sheet excel support

--- skinit/skread/read_dir_xlsx.r
+++ skinit/skread/read_dir_xlsx.r
@@ -14,19 +14,25 @@ read_dir_xlsx <- function(dir) {
     filenames <- list.files(dir)
     only_xlsx <- str$str_detect(filenames, ".+\\.xlsx$")
     no_suffix_xlsx <- str$str_remove(filenames[only_xlsx], ".xlsx$")
-
     if (all(!only_xlsx)) {
         cli$cli_alert_info("No xlsx files found in {.path {dir}}.")
         return(NULL)
     }
-
     list_df_xlsx <-
         no_suffix_xlsx %>%
         pr$set_names() %>%
-        pr$map(~ tbl$as_tibble(xl$read_xlsx(paste0(dir, .x, ".xlsx"))))
-
+        pr$map(function(x) {
+            path <- paste0(dir, x, ".xlsx")
+            sheets <- xl$excel_sheets(path)
+            if (length(sheets) == 1L) {
+                tbl$as_tibble(xl$read_xlsx(path))
+            } else {
+                sheets %>%
+                    pr$set_names() %>%
+                    pr$map(\(sheet) tbl$as_tibble(xl$read_xlsx(path, sheet = sheet)))
+            }
+        })
     dur <- round(lub$as.duration(Sys.time() - start), 2)
     cli$cli_alert_success("Read {length(list_df_xlsx)} excel files in {dur}.")
-
     list(data = list_df_xlsx, names = no_suffix_xlsx)
 }
